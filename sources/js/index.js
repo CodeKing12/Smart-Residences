e632ed7ab7b34318dca102e3757792a0a7432829
Kingsley <eyetukingsley330@gmail.com>
2022-04-07 14:36:47 +0100

    I have fixed the sliding for the carousel and added hover attributes to the play button on the hero page.

--- sources/js/index.js
+++ sources/js/index.js
@@ -2,13 +2,33 @@ $(document).ready(function() {
     $(".header-slick").slick({
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 2800,
+        autoplaySpeed: 3200,
         fade: true,
-        cssEase: 'linear',
         dots: false,
         speed: 500,
         arrows: false,
         zIndex: 100,
-        draggable: true,
+        asNavFor: '.image-slider',
+        // variableWidth: true,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+    });
+
+    $(".image-slider").slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3200,
+        fade: true,
+        dots: false,
+        speed: 500,
+        arrows: false,
+        zIndex: 100,
+        asNavFor: '.header-slick',
     })
+
+    // $(".owl-header").owlCarousel({
+    //     items: 1,
+    //     animateOut: 'fadeOut',
+    //     loop: true,
+    // })
 })
