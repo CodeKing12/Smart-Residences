3158b4defc44df3a201020e604c3c6534367c8e6
Kingsley <eyetukingsley330@gmail.com>
2022-04-07 12:35:20 +0100

    I am done with setting up tailwind css and creating the landing page for the Smart Residence website.

---
+++ sources/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".header-slick").slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2800,
+        fade: true,
+        cssEase: 'linear',
+        dots: false,
+        speed: 500,
+        arrows: false,
+        zIndex: 100,
+        draggable: true,
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./sources/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        rancho: ['Rancho', 'serif'],
+      },
+      'colors': {
+        'purple': '#5F0766',
+        'orange': '#FFC200',
+      },
+    },
+  },
+  plugins: [],
+}
