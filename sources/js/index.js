211e6c7278478a081fc017f2cf4672b1c9771878
Kingsley <eyetukingsley330@gmail.com>
2022-04-09 20:29:15 +0100

    I have completed the pricing page and made it responsive.

--- sources/js/index.js
+++ sources/js/index.js
@@ -86,32 +86,10 @@ $(document).ready(function() {
 
 });
 
-console.clear();
+function openMenu() {
+    document.getElementById("menu").style.height = "100%";
+}
 
-const app = (() => {
-	let body;
-	let menu;
-	let menuItems;
-	
-	const init = () => {
-		body = document.querySelector('body');
-		menu = document.querySelector('.menu-icon');
-		menuItems = document.querySelectorAll('.nav__list-item');
-
-		applyListeners();
-	}
-	
-	const applyListeners = () => {
-		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
-	}
-	
-	const toggleClass = (element, stringClass) => {
-		if(element.classList.contains(stringClass))
-			element.classList.remove(stringClass);
-		else
-			element.classList.add(stringClass);
-	}
-	
-	init();
-})();
-
+function closeMenu() {
+    document.getElementById("menu").style.height = "0";
+}
